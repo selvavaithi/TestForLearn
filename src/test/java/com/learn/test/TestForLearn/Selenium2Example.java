6d4f62d44d20c38243bee40fffc91a38e0a47840
selvavaith <selvav@softcrylic.co.in>
2016-04-22 16:08:09 +0530

    pom
    pom change

--- src/test/java/com/learn/test/TestForLearn/Selenium2Example.java
+++ src/test/java/com/learn/test/TestForLearn/Selenium2Example.java
@@ -1,5 +1,8 @@
 package com.learn.test.TestForLearn;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,42 +11,42 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Selenium2Example {
-	
-    public static void main(String[] args) {
-        // Create a new instance of the Firefox driver
-        // Notice that the remainder of the code relies on the interface, 
-        // not the implementation.
-        WebDriver driver = new FirefoxDriver();
+	static WebDriver driver;
 
-        // And now use this to visit Google
-        driver.get("http://www.google.com");
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.google.com");
+	@BeforeClass
+	public static void setUp() {
+		driver = new FirefoxDriver();
+		driver.get("http://www.google.com");
+	}
 
-        // Find the text input element by its name
-        WebElement element = driver.findElement(By.name("q"));
+	@Test
+	public void testing() {
 
-        // Enter something to search for
-        element.sendKeys("Cheese!");
+		WebElement element = driver.findElement(By.name("q"));
 
-        // Now submit the form. WebDriver will find the form for us from the element
-        element.submit();
+		// Enter something to search for mvn mvn -Dtest=**/Selenium2Example test
+		element.sendKeys("Cheese!");
 
-        // Check the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
-        
-        // Google's search is rendered dynamically with JavaScript.
-        // Wait for the page to load, timeout after 10 seconds
-        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return d.getTitle().toLowerCase().startsWith("cheese!");
-            }
-        });
+		// Now submit the form. WebDriver will find the form for us from the
+		// element
+		element.submit();
 
-        // Should see: "cheese! - Google Search"
-        System.out.println("Page title is: " + driver.getTitle());
-        
-        //Close the browser
-        driver.quit();
-    }
+		// Check the title of the page
+		System.out.println("Page title is: " + driver.getTitle());
+
+		// Google's search is rendered dynamically with JavaScript.
+		// Wait for the page to load, timeout after 10 seconds
+		(new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver d) {
+				return d.getTitle().toLowerCase().startsWith("cheese!");
+			}
+		});
+
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		System.out.println("Page title is: " + driver.getTitle());
+		driver.quit();
+	}
 }
